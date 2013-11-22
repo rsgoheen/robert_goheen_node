2395f84bfab60dbfd45f76b82528b74946abd733
Robert Goheen <goheen@gmail.com>
2013-11-22 22:01:02 +0000

    Full framework

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 var express = require('express');
 var app = express();
-var mustache = require('mustache');
+var path = require('path');
 
-app.get ('/', function(req, res) {
-    res.send("Hello app");
-});
+var routes = require('./routes');
 
-var port = 27534;
+app.set('port', process.env.PORT || 16541);
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'mustache');
+app.engine('mustache', require('hogan-middleware').__express)
+
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.methodOverride());
+app.use(app.router);
+//app.use(require('less-middleware')({ src: path.join(__dirname, 'public') }));
+app.use(express.static(path.join(__dirname, 'static')));
+
+app.get ('/', routes.index);
+
+var port = app.get('port');
 app.listen(port);
 console.log("Started listening on port ", port);
